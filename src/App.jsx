334d1d8cb9831433a4c8e7eb9b3d4ee4b009de27
otofu-square <yukita@feedforce.jp>
2017-06-02 23:43:52 +0900

    :wave: Remove unecessary files

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,10 @@
 // @flow
 
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 const App = () => (
-  <div className="App">
-    <div className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h2>Welcome to React</h2>
-    </div>
-    <p className="App-intro">
-      To get started, edit <code>src/App.js</code> and save to reload.
-    </p>
+  <div>
+    <h1>Hello World!!</h1>
   </div>
 );
 
