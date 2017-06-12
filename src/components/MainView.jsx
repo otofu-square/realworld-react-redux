34c620cf43254081611b8ff0ab5499c9f74d1eec
otofu-square <yukita@feedforce.jp>
2017-06-12 23:08:33 +0900

    :+1: Add MainView component

---
+++ src/components/MainView.jsx
@@ -0,0 +1,21 @@
+// @flow
+
+import React from 'react';
+
+import ArticleList from './ArticleList';
+
+const MainView = () =>
+  <div className="col-md-9">
+    <div className="feed-toggle">
+      <ul className="nav nav-pills outline-active">
+        <li className="nav-item">
+          <a href="" className="nav-link active">
+            Global Feed
+          </a>
+        </li>
+      </ul>
+    </div>
+    <ArticleList />
+  </div>;
+
+export default MainView;
