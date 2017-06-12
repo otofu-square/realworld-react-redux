76a3f9373a2756d2b5470e507dcd86217584abdd
otofu-square <yukita@feedforce.jp>
2017-06-12 23:07:47 +0900

    :+1: Add ArticleList component

---
+++ src/components/ArticleList.jsx
@@ -0,0 +1,21 @@
+// @flow
+
+import React from 'react';
+
+type Props = {
+  articles: Array,
+};
+
+const ArticleList = ({ articles }: Props) => {
+  if (!articles) return <div className="article-preview">Loading...</div>;
+  if (articles.length === 0) {
+    return <div className="article-preview">No articles are here... yet.</div>;
+  }
+  return (
+    <div>
+      {articles.map(article => <h2>{article.title}</h2>)}
+    </div>
+  );
+};
+
+export default ArticleList;
