f427ccaedb132ecee9e23591eac7f06c1606ba16
otofu-square <yukita@feedforce.jp>
2017-07-04 23:40:27 +0900

    :tada: Introduce first typescript component

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// @flow
+
+import * as React from 'react';
+
+// import Header from './components/Header';
+// import Home from './containers/Home';
+
+const App = () => <h1>Hello World!!</h1>;
+// <div>
+//   <Header appName={'Conduit'} />
+//   <Home />
+// </div>;
+
+export default App;
---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import * as thunk from 'redux-thunk';
+
+// import './index.css';
+import App from './App';
+
+// import reducer from './reducers/reducer';
+
+// const store = createStore(reducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+  // <Provider store={store}>
+  <App />,
+  // </Provider>,
+  document.getElementById('root'),
+);
