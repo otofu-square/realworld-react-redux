79984192f9017fd92536e467003b96dbaa3d72f9
otofu-square <yukita@feedforce.jp>
2017-07-05 01:32:53 +0900

    :+1: Add models

---
+++ src/models/author.ts
@@ -0,0 +1,6 @@
+export interface Author {
+  bio: string;
+  following: boolean;
+  image: string;
+  username: string;
+}
---
+++ src/models/state.ts
@@ -0,0 +1,5 @@
+import { Article } from './article';
+
+export interface articleListState {
+  articles: Array<Article> | null;
+}
