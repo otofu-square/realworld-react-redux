c331b80a52640164ad33d9a6f1fa8583bf78c58b
otofu-square <yukita@feedforce.jp>
2017-07-05 22:30:20 +0900

    :+1: Improve menu header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 
 interface Props {
   appName: string;
@@ -7,9 +8,16 @@ interface Props {
 const Header: React.StatelessComponent<Props> = ({ appName }) =>
   <nav className="navbar navbar-light">
     <div className="container">
-      <a className="navbar-brand">
+      <Link to="/" className="navbar-brand">
         {appName.toLowerCase()}
-      </a>
+      </Link>
+      <ul className="nav navbar-nav pull-xs-right">
+        <li className="nav-item">
+          <Link to="/login" className="nav-link">
+            Sign in
+          </Link>
+        </li>
+      </ul>
     </div>
   </nav>;
 
