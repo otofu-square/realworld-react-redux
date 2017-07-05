45f4a9e410735ba9115e3cc08b4a4ea09d83d3ab
otofu-square <yukita@feedforce.jp>
2017-07-05 22:30:10 +0900

    :+1: Add Login page

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { HashRouter, Route, Switch } from 'react-router-dom';
 
 import Header from './components/Header';
 import Home from './containers/Home';
+import Login from './pages/Login';
 
 const App = () =>
   <HashRouter>
@@ -10,6 +11,7 @@ const App = () =>
       <Header appName={'Conduit'} />;
       <Switch>
         <Route exact path="/" component={Home as any} />
+        <Route path="/login" component={Login as any} />
       </Switch>
     </div>
   </HashRouter>;
