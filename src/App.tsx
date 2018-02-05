447a3882418897c91c7f0e7f8c7189b093a6a563
otofu-square <yukita@feedforce.jp>
2018-02-05 23:49:22 +0900

    :+1: Add App container

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,40 @@
 import * as React from 'react';
+import styled from 'styled-components';
+import { Button } from 'antd';
 
-export const App = () => <h1>Hello World</h1>;
+export type ViewProps = {
+  count: number;
+};
+
+export type ActionProps = {
+  onAddClick: () => void;
+  onSubClick: () => void;
+};
+
+type Props = ViewProps & ActionProps;
+
+const StyledHeader = styled.h1`
+  text-align: center;
+  margin: 10px;
+`;
+
+const StyledDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  button {
+    margin: 5px;
+  }
+`;
+
+export const App = ({ count, onAddClick, onSubClick }: Props) => (
+  <>
+    <StyledHeader>Count: {count}</StyledHeader>
+    <StyledDiv>
+      {/* tslint:disable no-console */}
+      <Button onClick={onAddClick}> + </Button>
+      <Button onClick={onSubClick}> - </Button>
+    </StyledDiv>
+  </>
+);
