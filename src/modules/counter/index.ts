7f151c9980f9451da481a377d0251a0c020b95e1
otofu-square <yukita@feedforce.jp>
2018-02-17 16:20:50 +0900

    :wave: Remove unnecessary counter

--- src/modules/counter/index.ts
+++
@@ -1,6 +0,0 @@
-export {
-  actions as counterActions,
-  Action as CounterAction,
-  Actions as CounterActions,
-} from './action';
-export { reducer as counterReducer, State as CounterState } from './reducer';
