400fb2100fd447fde7b5def47b914de39a5d7f63
otofu-square <yukita@feedforce.jp>
2018-02-17 16:13:32 +0900

    :+1: Add Login & NotFound component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,38 @@
 import * as React from 'react';
 import { translate, InjectedTranslateProps } from 'react-i18next';
 
-type Props = InjectedTranslateProps;
+export type ViewProps = {};
+export type ActionProps = {
+  onLogoClick: () => void;
+  onHomeClick: () => void;
+  onLoginClick: () => void;
+};
+type Props = ViewProps & ActionProps;
+type ComposedProps = Props & InjectedTranslateProps;
 
-const Component = ({ t }: Props) => (
+const Component = ({
+  onLogoClick,
+  onHomeClick,
+  onLoginClick,
+  t,
+}: ComposedProps) => (
   <nav className="navbar navbar-light">
     <div className="container">
-      <a className="navbar-brand">{t('common.appName')}</a>
+      <div className="navbar-brand" onClick={onLogoClick}>
+        {t('common.appName')}
+      </div>
+      <ul className="nav navbar-nav pull-xs-right">
+        <li className="nav-item">
+          <div className="nav-link" onClick={onHomeClick}>
+            {t('common.home')}
+          </div>
+        </li>
+        <li className="nav-item">
+          <div className="nav-link" onClick={onLoginClick}>
+            {t('common.login')}
+          </div>
+        </li>
+      </ul>
     </div>
   </nav>
 );
