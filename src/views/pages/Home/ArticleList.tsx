10ff6d2c561a9593ce104c5745e5daf889bec40b
otofu-square <otofu.square@gmail.com>
2019-05-02 18:15:27 +0900

    :recycle: goodbye recompose

--- src/views/pages/Home/ArticleList.tsx
+++ src/views/pages/Home/ArticleList.tsx
@@ -1,5 +1,4 @@
 import * as React from "react";
-import { branch, compose, renderComponent } from "recompose";
 import { useTranslation } from "react-i18next";
 import { Article } from "../../../modules/article";
 import { ArticlePreview } from "./ArticlePreview";
@@ -9,29 +8,23 @@ type Props = {
   loading: boolean;
 };
 
-const isLoading = ({ articles }: Props) => !articles;
-
-const isEmpty = ({ articles }: Props) => articles.length === 0;
-
-const Loading: React.FC = () => {
+export const ArticleList: React.FC<Props> = ({ articles, loading }) => {
   const { t } = useTranslation();
-  return <div className="article-preview">{t("common.loading")}</div>;
+  const isLoading = loading;
+  const isEmpty = articles.length === 0;
+
+  switch (true) {
+    case isLoading:
+      return <div className="article-preview">{t("common.loading")}</div>;
+    case isEmpty:
+      return <div className="article-preview">{t("article.empty")}</div>;
+    default:
+      return (
+        <div>
+          {articles.map(article => (
+            <ArticlePreview key={`article-${article.title}`} {...article} />
+          ))}
+        </div>
+      );
+  }
 };
-
-const Empty: React.FC = () => {
-  const { t } = useTranslation();
-  return <div className="article-preview">{t("article.empty")}</div>;
-};
-
-const List: React.FC<Props> = ({ articles }) => (
-  <div>
-    {articles.map(article => (
-      <ArticlePreview key={`article-${article.title}`} {...article} />
-    ))}
-  </div>
-);
-
-export const ArticleList = compose<Props, Props>(
-  branch(isLoading, renderComponent(Loading)),
-  branch(isEmpty, renderComponent(Empty))
-)(List);
